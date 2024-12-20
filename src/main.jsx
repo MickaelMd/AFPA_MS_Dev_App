1e66b7606d130936c55fb4c5f2b9cca01649fd1b
Mickael <mickaelmarchand@protonmail.com>
2024-12-20 10:54:44 +0100

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/test"
+          element={
+            <div>
+              <h1>Page de test</h1>
+              <Link to="/">Back</Link>
+            </div>
+          }
+        />
+      </Routes>
+      <Link to="/test">test</Link>
     </Router>
   </StrictMode>
 );
