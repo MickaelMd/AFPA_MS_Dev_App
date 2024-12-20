f9964491c212509c840d8356d0f25c9cc25b0b39
Mickael <mickaelmarchand@protonmail.com>
2024-12-20 10:42:56 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./assets/css/App.css";
-// import { useState } from "react";
+import PrenomNom from "./components/PrenomNom.jsx";
+import Compteur from "./components/Compteur.jsx";
+import Liste from "./components/Liste.jsx";
+import Moviedb from "./components/Moviedb.jsx";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <PrenomNom />
+      <Compteur />
+      <Liste />
+      <hr />
+      <Moviedb />
+    </>
+  );
 };
 
 export default App;
